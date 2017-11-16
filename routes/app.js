f363d3e11f1071f3bfbefcb7145b6c21f3fae257
jelitter <jelito@gmail.com>
2017-11-16 18:15:13 +0000

    added compression

--- routes/app.js
+++ routes/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 const chalk = require('chalk');
 
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
