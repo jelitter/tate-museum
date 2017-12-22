536969524eb51214ca8354b2512cc4608d0fd86b
Isaac Sanchez <jelito@gmail.com>
2017-12-22 11:00:32 +0000

    Added pagination to Shop

--- routes/models/artwork.js
+++ routes/models/artwork.js
@@ -5,97 +5,63 @@
 const express = require('express');
 const router = express.Router();
 const handleError = require('../../config/error.js');
+const User = require('../../models/user');
 const Artwork = require('../../models/Artwork.js');
 const searchArtworkByTitle = require('../../models/Artwork.js');
 const getArtwork = require('../../models/Artwork.js');
 
+const MAX_RESULTS = 8;
+let username = null;
 
-const MAX_RESULTS = 10;
-
-// Get Artworks
-router.get('/', (req, res, next) => {
-    res.format({
-        html: () => {
-            if (req.query.id !== undefined && req.query.id != "") {
-                Artwork.getArtworkById(req.query.id, function(err, artwork) {
-                    if (err) handleError(res, err);
-                    // else res.send(results);
-                    // else res.render('../views/partials/artwork.ejs', {
-                    else if (artwork) {
-                        const aw = [{
-                            thumbnailUrl: artwork.thumbnailUrl,
-                            title: artwork.title,
-                            artist: artwork.artist,
-                            id: artwork.id
-                        }];
-                        res.render('shop', { cache: true, data: aw });
-
-                    } else {
-                        next();
-                    }
-                });
-            } else {
-                // No artwork ID
-                Artwork.getArtwork(function(err, artwork) {
-                    if (err) handleError(res, err);
-                    else {
-                        res.render('shop', { cache: true, data: artwork });
-                    }
-                }, numberOfResults);
-            }
-        },
-        json: () => {
-
-            if (req.query.id !== undefined) {
-                Artwork.getArtworkById(req.query.id, function(err, artwork) {
-                    if (err) handleError(res, err);
-                    // else res.send(results);
-                    else res.render('../views/partials/artwork.ejs', {
-                        cache: true,
-                        thumbnailUrl: artwork.thumbnailUrl,
-                        title: artwork.title,
-                        artist: artwork.artist
-                    });
-                });
-            } else {
-                Artwork.getArtwork(function(err, results) {
-                    if (err) handleError(res, err);
-                    else res.send(results);
-                }, numberOfResults);
-            }
-        }
+router.post('*', loggedIn, (req, res, next) => {
+    User.findOne({ _id: req.session._id }, (err, user) => {
+        if (err) res.status(401).redirect('/');
+        if (user) {
+            username = user.username;
+            next();
+            // res.status(200).render('shop', { cache: true, data: { username: user.username, pagename: 'shop' } });
+        } else res.status(401).redirect('/');
+    });
+});
+router.get('*', loggedIn, (req, res, next) => {
+    User.findOne({ _id: req.session._id }, (err, user) => {
+        if (err) res.status(401).redirect('/');
+        if (user) {
+            username = user.username;
+            next();
+            // res.status(200).render('shop', { cache: true, data: { username: user.username, pagename: 'shop' } });
+        } else res.status(401).redirect('/');
     });
 });
 
-// Get Artwork by id
-router.get('/:id', (req, res, next) => {
-    const id = req.params.id
+loggedIn = function(req, res, next) {
+    if (req.session._id) return next();
+    else return res.redirect(401, '/');
+};
 
-    Artwork.getArtworkById(id, function (err, result) {
-        if (err) handleError(res, err);
-        else {
-            res.format({
-                json: () => {
-                    res.send(result);
-                },
-                html: () => {
-                    res.render('partials/artwork_b', {
-                        cache: true,
-                        data: {
-                            artwork: result
-                        }
-                    });
-                }
-            });
-        }
-    });
-});
+router.post('/search', (req, res, next) => {
 
-router.post('/search', (req,res,next) => {
-    
     const query = req.body.query;
+    const page = req.body.page | 1;
+
     console.log('Searching for', query, '...');
-    let re = new RegExp('.*'+ query + '.*', "i")
+    let re = new RegExp('.*' + query + '.*', "i")
+
+    if (query.length < 3) {
+        res.render('partials/shop_results', {
+            cache: true,
+            data: {
+                type: 'warning',
+                message: 'Please enter at least 3 characters',
+                total: 0,
+                page: 1,
+                pagename: 'shop',
+                query: query,
+                username: username
+            }
+        });
+        return;
+    }
 
     Artwork.find({ title: re }).count().exec((err, count) => {
         if (err) console.log("Couldn't get count.");
@@ -111,27 +77,59 @@ router.post('/search', (req,res,next) => {
                         data: {
                             artworks: results,
                             items: results.length,
-                            total: count
+                            total: count,
+                            page: page,
+                            pagename: 'shop',
+                            query: query,
+                            username: username
                         }
                     });
                 }
-            }).limit(MAX_RESULTS).skip(0);
+            }).limit(MAX_RESULTS).skip((page - 1) * MAX_RESULTS);
         }
     });
-
-
-    
-
-    
-
-    // searchArtworkByTitle(query, (err, result) => {
-    //     if (err) console.log('err', err);
-    //     else console.log('result', result);
-    // }, 10);
-
-    // getArtworkById = function (artworkid, callback) {
-    //     artworkModel.findOne({ id: artworkid }, callback);
-    // };
 });
+
+router.get('/search/:query', (req, res, next) => {
+    console.log('GET - params:', JSON.stringify(req.params));
+    res.redirect('/artwork/search/' + req.params.query + '/1');
+    // next();
+});
+
+router.get('/search/:query/:page', (req, res, next) => {
+    const query = req.params.query;
+    const page = req.params.page || 1;
+
+    console.log('GET - Searching for', query, ' - Page: ', page);
+    let re = new RegExp('.*' + query + '.*', "i")
+
+    Artwork.find({ title: re }).count().exec((err, count) => {
+        if (err) console.log("Couldn't get count.");
+        else {
+            const skip = (page - 1) * MAX_RESULTS;
+            console.log(`Count for ${query}: ${count}, Skip: ${skip}`);
+            Artwork.find({ title: re }, (err, results) => {
+                if (err) console.log('err', err);
+                else {
+                    console.log(results.length, ' items found for', query);
+                    res.render('shop', {
+                        cache: true,
+                        data: {
+                            artworks: results,
+                            items: results.length,
+                            total: count,
+                            page: page,
+                            pagename: 'shop',
+                            query: query,
+                            username: username
+                        }
+                    });
+                }
+            }).limit(MAX_RESULTS).skip(skip);
+        }
+    });
+});
+
+
 
 module.exports = router;
--- routes/notfound.js
+++ routes/notfound.js
@@ -5,15 +5,15 @@ let User = require('../models/user');
 router.get('/', (req, res) => {
     User.findOne({ _id: req.session._id }, (err, user) => {
         if (err) res.status(401).redirect('/');
-        
+
         res.status(404).render("../views/partials/error.ejs", {
             cache: true,
             data: {
                 error: 404,
                 errorMessage: "Not Found",
                 url: req.originalUrl,
-                username: user ? user.username : '', 
-                page: 'Not Found'
+                username: user ? user.username : '',
+                pagename: 'Not Found'
             }
         });
     });
