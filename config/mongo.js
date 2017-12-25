8c81ffde10cd0bf1b27ec270b0289361f4557068
Isaac <isaac.sanchez@mycit.ie>
2017-12-25 10:16:52 +0000

    Added config. vars for Heroku

--- config/mongo.js
+++ config/mongo.js
@@ -2,16 +2,14 @@ const mongocfg = {
     options: {
         useMongoClient: true,
         autoIndex: false,
-        // user: "user",
-        // pass: "tateuser557",
-        user: "admin",
-        pass: "tateadmin378",
+        user: process.env.MONGO_USERNAME,
+        pass: process.env.MONGO_PASSWORD,
         reconnectTries: Number.MAX_VALUE,
         keepAlive: 1,
         connectTimeoutMS: 30000,
         reconnectInterval: 1000,
     },
-    uri: 'mongodb://ds151355.mlab.com:51355/art'
+    uri: process.env.MONGO_URL
 };
 
 module.exports = mongocfg;
